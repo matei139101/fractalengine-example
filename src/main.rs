65a578472b922e623805bf32b03a65d04c340abd
Matei Petre <matei139101@gmail.com>
2026-09-24 14:30:29 +0200

    Update: Added example usage of the current DashboardService skeleton.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use fractalengine::{FractalEngine, ServiceRegistry};
+use fractalengine_dashboard::DashboardService;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut service_registry = ServiceRegistry::new();
+
+    let dashboard_service = DashboardService::new();
+    DashboardService::register(dashboard_service.clone(), &mut service_registry);
+    
+    let mut engine = FractalEngine::new(service_registry);
+    engine.run();
 }
